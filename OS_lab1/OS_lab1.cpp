d7a58a46974bdf3bcec07d4193d92b5a058c21e7
SerGenius1 <s.burnashev03@gmail.com>
2023-12-23 00:38:35 +0700

    Добавьте файлы проекта.

---
+++ OS_lab1/OS_lab1.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+#include <thread>
+#include <mutex>
+using namespace std;
+
+mutex mtx;
+condition_variable cv;
+bool flag = false;
+
+
+void producer()
+{
+	while (true)
+	{
+		this_thread::sleep_for(chrono::milliseconds(1000));
+		unique_lock<mutex> ul(mtx);
+		flag = true;
+		cout << "Событие отправлено\n";
+		cv.notify_one();
+		ul.unlock();
+	}
+}
+
+void consumer()
+{
+	while (true)
+	{
+		unique_lock<mutex> ul(mtx);
+		while (!flag) cv.wait(ul);
+		flag = false;
+		cout << "Событие обработано\n";
+	}
+}
+
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+	thread th1(producer);
+	thread th2(consumer);
+
+	th1.join();
+	th2.join();
+
+	return 0;
+}
